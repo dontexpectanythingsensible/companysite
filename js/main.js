abd26ffd16f7182f54773f29e2fd7e01424ea918
James Tease <james@jamestease.co.uk>
2018-03-12 16:15:07 +0000

    Adding video and styling

--- js/main.js
+++ js/main.js
@@ -1,4 +1,30 @@
-var backgrounds = 28;
-var chosen = Math.floor(Math.random() * backgrounds) + 1;
+function toggleProjects(e) {
+    e.preventDefault();
+    document.querySelector('.projects__older').classList.toggle('hidden');
+    document.querySelector('#showProjects').classList.add('hidden');
+}
 
-document.getElementsByTagName('body')[0].classList.add('body-' + chosen);
+function showOtherProjects(e) {
+    e.preventDefault();
+    document.querySelector('#otherProjects').classList.add('active');
+    document.querySelector('.projects__other').classList.remove('hidden');
+    document.querySelector('#games').classList.remove('active');
+    document.querySelector('.games').classList.add('hidden');
+}
+
+function showGames(e) {
+    e.preventDefault();
+    document.querySelector('#games').classList.add('active');
+    document.querySelector('.games').classList.remove('hidden');
+    document.querySelector('#otherProjects').classList.remove('active');
+    document.querySelector('.projects__other').classList.add('hidden');
+}
+
+const toggle = document.querySelector('#showProjects');
+toggle.addEventListener('click', toggleProjects);
+
+const projectToggle = document.querySelector('#otherProjects');
+projectToggle.addEventListener('click', showOtherProjects);
+
+const gameToggle = document.querySelector('#games');
+gameToggle.addEventListener('click', showGames);
